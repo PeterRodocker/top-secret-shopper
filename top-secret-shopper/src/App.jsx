ca2ccd85c865f2124a84cbf0354b9c4e8f710287
James Scott <jsa.codes@gmail.com>
2023-09-03 13:50:18 -0500

    Accepted incoming change

--- top-secret-shopper/src/App.jsx
+++ top-secret-shopper/src/App.jsx
@@ -3,15 +3,9 @@ import Login from './components/login'
 
 function App() {
   return (
-<<<<<<< HEAD
-    
-      <Login />
-    
-=======
 
     <Login />
 
->>>>>>> 15587dcc1948524d11684288b1009e4f29eed8ba
   )
 }
 
