59533c1943f1e704cff9a7f899364256eeb6388b
James Scott <jsa.codes@gmail.com>
2023-09-03 14:03:05 -0500

    Fixing merge conflict

--- top-secret-shopper/src/App.jsx
+++ top-secret-shopper/src/App.jsx
@@ -3,13 +3,9 @@ import Login from './components/login'
 
 function App() {
   return (
-<<<<<<< HEAD
 
     <Login />
 
-=======
-      <Login />
->>>>>>> 38123c813b6910152cdcd568c2b426505333a1e6
   )
 }
 
