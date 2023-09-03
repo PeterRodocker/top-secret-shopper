99c0781b1f9f6ad2fa3d766d94f480876e82f8b2
James Scott <jsa.codes@gmail.com>
2023-09-03 14:53:21 -0500

    Fixed incoming changes

--- top-secret-shopper/src/components/login.jsx
+++ top-secret-shopper/src/components/login.jsx
@@ -1,57 +1,8 @@
-<<<<<<< HEAD
-import React, {useState} from 'react'
-=======
 import React, { useState } from 'react'
->>>>>>> 15587dcc1948524d11684288b1009e4f29eed8ba
 import axios from 'axios'
 import { Button, Form } from 'semantic-ui-react'
 
 const Login = () => {
-<<<<<<< HEAD
-    
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-
-    const firstName = username
-    const lastName = password
-
-
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        // axios.post('https://64e78e68b0fd9648b7901b1e.mockapi.io/fakedata', {
-        //     username,
-        //     password
-        // })
-
-        axios.post('https://64e78e68b0fd9648b7901b1e.mockapi.io/fakedata', {
-            firstName,
-            lastName
-        })
-    }
-
-    
-    
-    return (
-    <div>
-        <Form>
-            <Form.Field>
-                <label>Username</label>
-                <input placeholder='username' onChange={(e) => setUsername(e.target.value)}/>
-            </Form.Field>
-            <Form.Field>
-                <label>Password</label>
-                <input placeholder='password' onChange={(e) => setPassword(e.target.value)}/>
-            </Form.Field>
-            <Button type='submit' onClick={handleSubmit}>Submit</Button>
-        </Form>
-    </div>
-    )
-
-}
-
-export default Login
-
-=======
 
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
@@ -90,4 +41,3 @@ export default Login
 }
 
 export default Login
->>>>>>> 15587dcc1948524d11684288b1009e4f29eed8ba
