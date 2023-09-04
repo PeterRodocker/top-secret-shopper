a68d71ad56af9a36e06b82e199a9e16a3a6fe754
Peter Rodocker <107810098+PeterRodocker@users.noreply.github.com>
2023-09-04 10:40:35 -0700

    Commit with conflicts

--- top-secret-shopper/src/App.jsx
+++ top-secret-shopper/src/App.jsx
@@ -3,15 +3,9 @@ import Login from './components/login'
 
 function App() {
   return (
-<<<<<<<< < Temporary merge branch 1
-
-    < Login />
-
-=========
 
     <Login />
 
->>>>>>>>> Temporary merge branch 2
   )
 }
 
