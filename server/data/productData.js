926be025d67f355f99c5514a3a5f1462c28d20ce
Peter Rodocker <peterrodocker@gmail.com>
2024-11-13 20:46:59 -0800

    Update Product Data

--- server/data/productData.js
+++ server/data/productData.js
@@ -2,8 +2,8 @@ const { faker } = require('@faker-js/faker');
 
 const productData = [
   {
-    name: 'Cauliflower',
-    price: 4.99,
+    name: 'Documents',
+    price: 5,
     // imageURL: faker.image.urlLoremFlickr({ category: 'abstract' }),
     imageURL: faker.image.urlLoremFlickr({ category: 'abstract' }),
     stockQty: 30,
@@ -12,8 +12,8 @@ const productData = [
   },
 
   {
-    name: 'Taco',
-    price: 1.99,
+    name: 'X-Ray Glasses',
+    price: 2,
     imageURL: faker.image.urlLoremFlickr({ category: 'abstract' }),
     stockQty: 25,
     categoryId: 1,
@@ -22,21 +22,45 @@ const productData = [
   },
 
   {
-    name: 'Pizza',
-    price: 49.99,
+    name: 'Poison',
+    price: 50,
     imageURL: faker.image.urlLoremFlickr({ category: 'abstract' }),
     stockQty: 10,
     categoryId: 2,
     description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero quo, placeat harum cupiditate quaerat dolore, quidem velit soluta magnam itaque modi aut distinctio unde omnis. Iusto modi vitae corporis iste.'
   },
   {
-    name: 'Rice',
-    price: 19.99,
+    name: 'Smoke Bombs',
+    price: 20,
     imageURL: faker.image.urlLoremFlickr({ category: 'abstract' }),
     stockQty: 85,
     categoryId: 3,
     description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero quo, placeat harum cupiditate quaerat dolore, quidem velit soluta magnam itaque modi aut distinctio unde omnis. Iusto modi vitae corporis iste.'
-  }
+  },
+  {
+    name: 'Lies',
+    price: 15,
+    imageURL: faker.image.urlLoremFlickr({ category: 'abstract' }),
+    stockQty: 12,
+    categoryId: 3,
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero quo, placeat harum cupiditate quaerat dolore, quidem velit soluta magnam itaque modi aut distinctio unde omnis. Iusto modi vitae corporis iste.'
+  },
+  {
+    name: 'Fedora',
+    price: 25,
+    imageURL: faker.image.urlLoremFlickr({ category: 'abstract' }),
+    stockQty: 18,
+    categoryId: 1,
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero quo, placeat harum cupiditate quaerat dolore, quidem velit soluta magnam itaque modi aut distinctio unde omnis. Iusto modi vitae corporis iste.'
+  },
+  {
+    name: 'Trench Coat',
+    price: 90,
+    imageURL: faker.image.urlLoremFlickr({ category: 'abstract' }),
+    stockQty: 4,
+    categoryId: 2,
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero quo, placeat harum cupiditate quaerat dolore, quidem velit soluta magnam itaque modi aut distinctio unde omnis. Iusto modi vitae corporis iste.'
+  },
 ]
 
 module.exports = productData;
