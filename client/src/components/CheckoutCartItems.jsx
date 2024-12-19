a6dcb6d39c1a77a024c2e85acec2bcf1ad11a549
Peter Rodocker <peterrodocker@gmail.com>
2024-12-18 18:34:00 -0800

    Fixed CheckoutItem CSS

--- client/src/components/CheckoutCartItems.jsx
+++ client/src/components/CheckoutCartItems.jsx
@@ -6,11 +6,12 @@ function CheckoutCartItems({ cart }) {
   return (
     <div className="checkout__cart-items">
       <h3>Cart Items</h3>
-      <div className='checkout__items-container'>
-        {cart.length ? cart.map(cartItem => <CheckoutItemView
-          cartItem={cartItem}
-          key={cartItem.id}
-        />) :
+      <div className='checkout_items-container'>
+        {cart.length ? cart.map(cartItem => (
+          <CheckoutItemView
+            cartItem={cartItem}
+            key={cartItem.id}
+          />)) :
           <h3 className='no-items'>No items in your cart</h3>}
       </div>
     </div>
